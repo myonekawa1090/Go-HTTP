be092ef666b1b938ec8f6dbfd4ca9ea365313d6e
myonekawa1090 <bewith.1090@gmail.com>
2025-06-09 18:46:37 -0700

    add client.go

---
+++ client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"crypto/tls"
+	"crypto/x509"
+	"os"
+)
+
+func main () {
+	cert, err := os.ReadFile("ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	resp, err := client.Get("https://localhost:18443")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
